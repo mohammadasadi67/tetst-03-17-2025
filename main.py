44c0c6377a9e989da8bcad8488669684506121c6
mohammadasadi67 <mohammadasadollahzadeh67@gmail.com>
2025-03-23 09:35:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 import streamlit as st
+from pathlib import Path
 
-# Change layout between "wide" and "centered" to see the difference
-st.set_page_config(page_title="Test App")
+# Set page title
+st.set_page_config(page_title="My Streamlit App", layout="wide")
 
-st.title("Streamlit Layout Example")
-st.write("This content is centered because of `layout='centered'`.")
+# Sidebar navigation
+st.sidebar.title("Navigation")
+page = st.sidebar.radio("Go to", ["Home", "Page 1", "Page 2"])
 
-# Creating two columns to show the effect of centered layout
-col1, col2 = st.columns(2)
+# Home Page
+if page == "Home":
+    st.title("Welcome to My Streamlit App")
+    st.write("This is the home page.")
 
-with col1:
-    st.subheader("Column 1")
-    st.write("This is the first column.")
+# Page 1
+elif page == "Page 1":
+    st.title("Page 1")
+    st.write("This is the content of Page 1.")
 
-with col2:
-    st.subheader("Column 2")
-    st.write("This is the second column.")
-
-st.write("Try changing `layout='centered'` to `layout='wide'` and rerun the app to see the difference!")
+# Page 2
+elif page == "Page 2":
+    st.title("Page 2")
+    st.write("This is the content of Page 2.")
